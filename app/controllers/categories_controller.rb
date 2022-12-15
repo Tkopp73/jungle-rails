e0cbd73025683eb5f023da7ce314d4c2733200c6
Tim Kopplin <timkopplin@hotmail.com>
2022-12-15 00:40:24 +0000

    adding to the dashboard page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+  has_many :products
+  
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
