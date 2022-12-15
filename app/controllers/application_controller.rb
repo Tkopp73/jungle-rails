addfdb5ae92e938caa5db03ca973676ebbb5f918
Tim Kopplin <timkopplin@hotmail.com>
2022-12-15 20:46:18 +0000

    adding the login and logout function for the site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,15 @@ class ApplicationController < ActionController::Base
 
   private
 
+  helper_method :current_user
+
+  def current_user
+    # Look up the current user based on user_id in the session cookie:
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+
+
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'users/new' => 'users#new', as: :new_user
+  post 'users' => 'users#create'
+  get 'users/create'
+
+  get '/login'     => 'sessions#new'
+  post '/login'    => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'  
+
+
+
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
